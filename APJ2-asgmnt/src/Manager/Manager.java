0198cbba452be859416a70e702d68bffda0f8753
m.tan <tannm@smartosc.com>
2014-02-25 14:24:34 +0700

    :D

---
+++ APJ2-asgmnt/src/Manager/Manager.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Manager;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tan
+ */
+public class Manager {
+    private ArrayList list;
+    
+    
+    public Class getType(Object object){
+        return object.getClass();
+    }
+}
---
+++ APJ2-asgmnt/src/Models/Student.java
@@ -0,0 +1,23 @@
+package Models;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tan
+ */
+public class Student {
+    private int studentID;
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+}
