ac28be748fbc8aacd5246c4fe6711243be026262
m.tan <m.tan2007@gmail.com>
2014-03-04 11:50:02 +0700

    :D

--- APJ2-asgmnt/src/DatabaseDriver/DatabaseDriver.java
+++ APJ2-asgmnt/src/DatabaseDriver/DatabaseDriver.java
@@ -4,10 +4,63 @@
  */
 package DatabaseDriver;
 
+import com.sun.rowset.CachedRowSetImpl;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.rowset.CachedRowSet;
+
 /**
  *
  * @author Tan
  */
 public class DatabaseDriver {
     private String server;
+    private String username;
+    private String psw;
+    private CachedRowSet crs;
+
+    public DatabaseDriver(String server, String username, String psw) {
+        this.server = server;
+        this.username = username;
+        this.psw = psw;
+        try {
+            this.crs = new CachedRowSetImpl();
+            this.crs.setUrl("jdbc:sqlserver://" + this.server + ";Database=db_student");
+            this.crs.setUsername(this.username);
+            this.crs.setPassword(this.psw);
+            this.crs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseDriver.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+    public selectFrom(String table){
+    
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+    
 }
